93a599e1e7954ac1f00ef8315bd8db231089c641
JemmyKibandi <jemimakibandi378@gmail.com>
2023-08-29 10:51:48 +0300

    Component creation and rendering

---
+++ app-name/resources/views/components/button.blade.php
@@ -0,0 +1,5 @@
+<div>
+<button class="bg-yellow-400 text-white text-medium px-4 py-2 rounded-md hover:bg-yellow-500">
+            Click Me
+        </button><!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+</div>
