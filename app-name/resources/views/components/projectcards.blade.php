031d3275c5707dc8e2d250fa9df81b12bbc2a3cb
JemmyKibandi <jemimakibandi378@gmail.com>
2023-09-06 16:58:53 +0300

    MORE

--- app-name/resources/views/components/projectcards.blade.php
+++ app-name/resources/views/components/projectcards.blade.php
@@ -21,16 +21,19 @@
   <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
     <div class="card bg-base-100 shadow-xl">
     <figure class="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    <img src="{{ asset('images/js.jpg') }}"  alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">JAVASCRIPT</h2>
+    <p>
+This is all my work with Javascript<BR><BR><BR>
+    </p>
     <div class="card-actions">
-      <button class="btn btn-primary">Click to view</button>
+      <a class="btn btn-primary" target="blank" href="https://github.com/JemmyKibandi/js-trials" target="blank">Click to view</a>
     </div>
   </div>    </div>
   </div>
+  
 
   <!-- Third Card -->
   <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
