6957ffb77fa1c1081d5d26cf6de0be29231b6895
JemmyKibandi <jemimakibandi378@gmail.com>
2023-09-06 16:05:58 +0300

    more

--- app-name/resources/views/components/inout.blade.php
+++ app-name/resources/views/components/inout.blade.php
@@ -7,16 +7,17 @@
     <p id="paragraph1">Paragraph 1</p>
     <p id="paragraph2">Paragraph 2</p> -->
 
-    <div class="hero h-[50vh] relative" style="background-image: url(https://media.istockphoto.com/id/1173935107/photo/long-wave-on-the-coast-dawn-on-the-sea-tunisia.jpg?s=612x612&w=0&k=20&c=Q-O-34NjJpbiOtNlGGUGEllKATijp34X_xfZphY7VtE=);">
-  <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero h-[50vh] relative">
+     <!-- style="background-image: url(https://media.istockphoto.com/id/1173935107/photo/long-wave-on-the-coast-dawn-on-the-sea-tunisia.jpg?s=612x612&w=0&k=20&c=Q-O-34NjJpbiOtNlGGUGEllKATijp34X_xfZphY7VtE=);"> -->
+  <div class="hero"></div>
   <div class="hero-content text-center text-neutral-content">
     <div class="max-w-md">
-      <h1 id="header" class="mb-5 text-5xl font-bold">Oh Hi there 👀!</h1>
-      <p id="paragraph1" class="mb-5">Jemima Kibandi is an enthusiastic individual dedicated to expanding my knowledge and skills in web development.
+      <h1 id="header" class="mb-5 text-5xl text-black font-bold">Oh Hi there 👀!</h1>
+      <p id="paragraph1" class="mb-5 text-black">Jemima Kibandi is an enthusiastic individual dedicated to expanding my knowledge and skills in web development.
         I have sought assistance in diverse areas, including Laravel, Tailwind CSS, GSAP animations,
           and more. <br>Look at my Projects below 😁
     </p>
-   <center> <button class="btn btn-primary flex items-center">
+   <center> <button class="btn text-black btn-primary flex items-center">
   Scroll to see More
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
     <path d="M7.646 10.854a.5.5 0 00.708 0l4-4a.5.5 0 00-.708-.708L8 9.293l-3.646-3.647a.5.5 0 00-.708.708l4 4a.5.5 0 000 .708z"/>
--- app-name/resources/views/components/projectcards.blade.php
+++ app-name/resources/views/components/projectcards.blade.php
@@ -5,13 +5,14 @@
   <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
     <div class="card bg-base-100 shadow-xl">
     <figure class="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    <img src="{{ asset('images/yes.png') }}" alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">Hope For Life CBO</h2>
+    <p>
+    Hope for Life CBO, a registered Foundation in Western Kenya, focuses on Poverty Eradication, Education, and supporting Widows living with HIV/AIDS.    </p>
     <div class="card-actions">
-      <button class="btn btn-primary">Click to View</button>
+      <a target="blank" class="btn btn-primary" href="https://hopeforlifecbo.co.ke/">Click to View</a>
     </div>
   </div>    </div>
   </div>
