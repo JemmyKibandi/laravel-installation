c95f50d7a8d4350bceee0a84171237efce496992
JemmyKibandi <jemimakibandi378@gmail.com>
2023-09-08 14:37:24 +0300

    Updates

--- app-name/resources/views/components/projectcards.blade.php
+++ app-name/resources/views/components/projectcards.blade.php
@@ -26,7 +26,8 @@
   <div class="card-body items-center text-center">
     <h2 class="card-title">JAVASCRIPT</h2>
     <p>
-This is all my work with Javascript<BR><BR><BR>
+This is all my work with Javascript
+<BR><BR><BR><BR> <BR> 
     </p>
     <div class="card-actions">
       <a class="btn btn-primary" target="blank" href="https://github.com/JemmyKibandi/js-trials" target="blank">Click to view Git repository</a>
@@ -39,13 +40,18 @@ This is all my work with Javascript<BR><BR><BR>
   <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4">
     <div class="card bg-base-100 shadow-xl">
     <figure class="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    <img src="{{ asset('images/php.png') }}"  alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">PHP!</h2>
+    <p>
+    This is all my work with PHP
+<BR><BR><BR> <BR><BR><BR> <BR><BR><BR>    
+   
+   
+</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Click to view</button>
+      <button class="btn btn-primary">Click to view Git repository</button>
     </div>
   </div>    </div>
   </div>
